96b2b6ac3650d941d05b50ffdaf4ab2347191edd
EagleMind <hassen.ben.mbarek@gmail.com>
2022-03-29 19:43:13 +0200

    signup design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    DividerModule,
+    ButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
